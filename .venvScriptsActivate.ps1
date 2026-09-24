ac8babcf02ffa409dffa587fc2c48aec82d83e4d
KanrabitChala <kanichalakani@gmail.com>
2026-09-24 14:22:13 +0300

    initial project

---
+++ .venvScriptsActivate.ps1
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     2041       1    2041       9344  cons0     197609 08:58:22 /usr/bin/bash
+     2279    2041    2279       7680  cons0     197609 10:35:27 /usr/bin/PS
+     2153       1    2153      10968  cons1     197609 10:34:54 /usr/bin/bash
---
+++ model.py
@@ -0,0 +1,47 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(16 * 14 * 14, 10)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = x.view(x.size(0), -1)
+        return self.fc1(x)
+
+def train_model():
+    if os.path.exists("model.pth"):
+        print("[*] Model weights already exist.")
+        return
+
+    print("[*] Training baseline model on MNIST...")
+    transform = transforms.Compose([transforms.ToTensor()])
+    dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    loader = DataLoader(dataset, batch_size=64, shuffle=True)
+
+    model = SimpleCNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+    model.train()
+    for images, labels in loader:
+        optimizer.zero_grad()
+        loss = criterion(model(images), labels)
+        loss.backward()
+        optimizer.step()
+        break  # Just 1 batch for instant local setup
+
+    torch.save(model.state_dict(), "model.pth")
+    print("[*] Model saved to model.pth")
+
+if __name__ == "__main__":
+    train_model()
